5f189bb014717891196031d1b44c0700cfb297c3
Tetiananalyvai <lady.nalivay@gmail.com>
2024-03-27 12:56:39 +0200

    punktid 1,2,3,5 on tehtud

---
+++ Moodul.py
@@ -0,0 +1,47 @@
+from os import remove, path,system
+from gtts import *
+
+def loe_failist(fail:str)->list:
+    """Loetakse informatsioon failist ja tagastatakse info nagu järjend
+    :param str fail: faili nimetus
+    .rtype: list
+    """
+    f=open(fail,'r',encoding="utf-8")
+    järjend=[]
+    for rida in f:
+        järjend.append(rida.strip())
+    f.close()
+    return järjend
+def tolkimine(sona:str,sonad:list,tolked:list)->str:
+    if sona in sonad:
+        indeks=sonad.index(sona)
+        return tolked[indeks]
+    elif sona in tolked:
+        indeks=tolked.index(sona)
+        return sonad[indeks]
+    else:
+        return "Sõna "+sona+" puudub"
+def teksti_lisamine_failisse(fail:str,sona:str,sonad:list):
+    sonad.append(sona)
+    f=open(fail,'a',encoding="utf-8")
+    f.write(sona+'\n')
+    f.close()
+    return sonad
+def faili_kustutamine():
+    faili_nimi=input("Mis fail on vaja kustutada ära?")
+    if path.isfile(faili_nimi): 
+       remove(faili_nimi) #path.isdir(kaust)
+       print(f"Fail {faili_nimi} oli kustutatud")
+    else:
+       print(f"Fail {faili_nimi} puudub")
+def räägimine(est:list,rus:list):
+    sona=input("Sisesta sõna:")
+    if sona in rus:
+        keel="ru"
+    elif sona in est:
+        keel="et"
+    if keel in ["ru","et"]: 
+        obj=gTTS(text=sona,lang=keel,slow=False).save("heli.mp3")
+        system("heli.mp3")
+    else:
+        print("Viga!")
--- Sonastik.py
+++ Sonastik.py
@@ -1,1 +1,31 @@
 
+from os import remove, path,system
+from Moodul import *
+
+
+rus=[] #vene kelsed
+est=[] #eestikeelsed sonad
+rus=loe_failist("Rus.txt")
+est=loe_failist("Est.txt")
+while True:
+    print(rus)
+    print(est)
+    v=input("1-Tõlkimine\n2-Sõnade Lisamine\n3-Kontroll\n4-Räägimine\n5-lõpp\n")
+    if v=="1":
+        print("Tolkimine")
+        sona=input("Sisasta sõna tõlkimiseks:")
+        tolke=tolkimine(sona,rus,est)
+        print(tolke)
+    elif v=="2":
+        print("Sõnade Lisamine")
+        est_sona=input("Sisesta sõna eesti keeles:")
+        rus_sona=input("Sisesta sõna vene keeles:")
+        est=teksti_lisamine_failisse("Est.txt",est_sona,est)
+        rus=teksti_lisamine_failisse("Rus.txt",rus_sona,rus)      
+    elif v=="3":
+        print("Teadmiste Kontroll")
+    elif v=="4":       
+        räägimine(est,rus)
+    elif v=="5":
+        print("Kõige head!")
+        break
